74162b1bdcf2f5cf40efd0ddb76498561279a265
kei224022 <ishikawa224022@protonmail.com>
2023-10-25 13:47:42 +0900

    comment

--- src/Main.java
+++ src/Main.java
@@ -10,20 +10,29 @@ public class Main {
         System.out.print("10進数の数字を入力してください: ");
         int decimal = scanner.nextInt();
 
+        //インスタンス化
         DecimalToBinary decimalToBinary = new DecimalToBinary();
+        //decimalToBinaryクラスからconvert関数を呼び出す
         String binary = decimalToBinary.convert(decimal);
 
+        //256以上なら計算不可
         if (binary.length() > 8) {
             System.out.println("エラー: 入力された数字は8桁の2進数に変換できません。");
         } else {
+            //正の数なら乗算、負の数なら除算
             System.out.print("シフトするビット数を入力してください（左シフトは正、右シフトは負の値）: ");
             int shiftAmount = scanner.nextInt();
 
+            //インスタンス化
             BinaryShift binaryShift = new BinaryShift();
+            //BinaryShiftクラスからshift関数の呼び出し
             String shiftedBinary = binaryShift.shift(binary, shiftAmount);
 
+            //インスタンス化
             BinaryToDecimal binaryToDecimal = new BinaryToDecimal();
+            //BinaryToDecimal関数からconvert関数を呼び出す
             int result = binaryToDecimal.convert(shiftedBinary);
+            //計算結果
             System.out.println("シフト後の10進数: " + result);
         }
 
